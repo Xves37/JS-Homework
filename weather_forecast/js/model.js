771edac3d75caffd4997fa38ef23ba2fcc813572
Daniil Khvesiuk <danik37h@gmail.com>
2018-11-21 09:22:51 +0300

    down grade for pull request

--- weather_forecast/js/model.js
+++ weather_forecast/js/model.js
@@ -1,46 +1,37 @@
-function weatherModel() {
-    
-    let view = weatherView();
-    let city = cityInput;
+let btn = document.querySelector('button.button');
+let cityInput = document.querySelector('.input-text input');
 
-    function weatherForecast() {
-        let city = getCity();
+const ENTER_KEY_CODE  = 13;
 
-        weatherToday(city);
+
+// test
+cityInput.value = 'homel';
+weatherForecast();
+// end test
+
+btn.addEventListener('click', () => {
+    weatherForecast();
+});
+
+cityInput.addEventListener('keypress', (e) => {    
+    if (e.keyCode == ENTER_KEY_CODE) {
+       weatherForecast();
     }
+});
 
-    function weatherToday(city) {
-        let request = new XMLHttpRequest();
-        let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5';
+function weatherForecast() {
+    let city = getCity();
 
-        request.onreadystatechange = function () {
-            let isRequestReady = request.readyState == 4;
-            let isCityCorrect = isRequestReady && request.status == 200;
-            let isCityIncorrect = isRequestReady && request.status == 404;
-            let isFieldEmpty = isRequestReady == 4 && request.status == 400;
+    weatherToday(city);
+    weatherWeek(city);
 
-            if (isCityCorrect) {
-                let responseJSON = request.responseText;
-                let response = JSON.parse(responseJSON);
-                
-                console.log(response);
-                view.changeCity(response);
-                view.changeWeatherToday(response);
-                // hideError();
-            } else if (isCityIncorrect) {
-                // showError('You entered wrong city!');
-            } else if (isFieldEmpty) {
-                // showError('Enter something!');
-            }
-        }
+}
 
-        request.open('GET', url);
-        request.send();
+function weatherToday(city) {
+    let request = new XMLHttpRequest();
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5';
 
-    }
-
-
-    function test(request, callback) {
+    request.onreadystatechange = function () {
         let isRequestReady = request.readyState == 4;
         let isCityCorrect = isRequestReady && request.status == 200;
         let isCityIncorrect = isRequestReady && request.status == 404;
@@ -50,7 +41,9 @@ function weatherModel() {
             let responseJSON = request.responseText;
             let response = JSON.parse(responseJSON);
             
-            callback();
+            console.log(response);            
+            changeCity(response);
+            changeWeatherToday(response);
             // hideError();
         } else if (isCityIncorrect) {
             // showError('You entered wrong city!');
@@ -59,51 +52,50 @@ function weatherModel() {
         }
     }
 
-    // function weatherWeek(city) {
-    //     let request = new XMLHttpRequest();
-    //     let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5';
-        
-    //     request.onreadystatechange = function () {
-    //         let isRequestReady = request.readyState == 4;
-
-
-    //         if (isRequestReady) {
-    //             let responseJSON = request.responseText;
-    //             let response = JSON.parse(responseJSON);
-                
-    //             showDates(response);
-    //             console.log(response);
-    //         }
-    //     }
-
-    //     request.open('GET', url);
-    //     request.send();
-    // }
-    
-    function getCity() {
-        let cityName = city.value;
-        cityName = deleteExtraSpaces(cityName);
-
-        return cityName;
-    }
-
-    function deleteExtraSpaces(str) {
-        let j = [];
-        let newStr = str.split('');
-
-        newStr.reduce( (last, element, i) => {
-            if (last == element && element == ' ') j.push(i);
-            return element;
-        })
-        for (let i = newStr.length - 1; i > 0; i--) {
-            if (j.indexOf(i) != -1) newStr.splice(i,1);
-        }
-
-        return newStr.join('').trim();
-    }
-
-    return {
-        weatherForecast: weatherForecast
-    }
-
+    request.open('GET', url);
+    request.send();
 }
+
+function weatherWeek(city) {
+    let request = new XMLHttpRequest();
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5';
+    
+    request.onreadystatechange = function () {
+        let isRequestReady = request.readyState == 4;
+
+
+        if (isRequestReady) {
+            let responseJSON = request.responseText;
+            let response = JSON.parse(responseJSON);
+            
+            // showDates(response);
+            console.log(response);
+        }
+    }
+
+    request.open('GET', url);
+    request.send();
+}
+
+function getCity() {
+    let cityName = cityInput.value;
+    cityName = deleteExtraSpaces(cityName);
+
+    return cityName;
+}
+
+function deleteExtraSpaces(str) {
+    let j = [];
+    let newStr = str.split('');
+
+    newStr.reduce( (last, element, i) => {
+        if (last == element && element == ' ') j.push(i);
+        return element;
+    })
+    for (let i = newStr.length - 1; i > 0; i--) {
+        if (j.indexOf(i) != -1) newStr.splice(i,1);
+    }
+
+    return newStr.join('').trim();
+}
+
--- weather_forecast/js/view.js
+++ weather_forecast/js/view.js
@@ -1,144 +1,137 @@
-function weatherView() {
-    let city  = document.querySelector('.city');
-    let week  = document.querySelector('.week');
-    let today = document.querySelector('.today');
+let city  = document.querySelector('.city');
+let week  = document.querySelector('.week');
+let today = document.querySelector('.today');
 
-    function changeCity (response) {
-        let cityName = response.name;
-        let countryCode = response.sys.country;
-        let coords = response.coord.lon + '; ' + response.coord.lat;
+function changeCity (response) {
+    let cityName = response.name;
+    let countryCode = response.sys.country;
+    let coords = response.coord.lon + '; ' + response.coord.lat;
 
-        let cityPattern = 
-            '<h2 class="city-name">' +
-                '<span class="name">' + cityName + '</span>,' +
-                '<span class="country"> ' + countryCode + '</span>' +
-            '</h2>' +
-            '<div class="coords">(' + coords + ')</div>' +
-            '<img class="flag" src="https://www.countryflags.io/' + countryCode + '/shiny/32.png"></img>';
+    let cityPattern = 
+        '<h2 class="city-name">' +
+            '<span class="name">' + cityName + '</span>,' +
+            '<span class="country"> ' + countryCode + '</span>' +
+        '</h2>' +
+        '<div class="coords">(' + coords + ')</div>' +
+        '<img class="flag" src="https://www.countryflags.io/' + countryCode + '/shiny/32.png"></img>';
 
-        city.innerHTML = cityPattern;    
-    }
+    city.innerHTML = cityPattern;    
+}
 
-    function changeWeatherToday(response) {
-        let temp = Math.round(response.main.temp - 273.15);
-        let date = (new Date).toString().slice(0,25);
-        let desc = response.weather[0].description.toString()[0].toUpperCase() + response.weather[0].description.slice(1);
-        let humidity = response.main.humidity;
-        let pressure = response.main.pressure;
-        let windSpeed = response.wind.deg;
-        let clouds = response.clouds.all;
-        let icon = parseImg(response.weather[0].icon);
-
+function changeWeatherToday(response) {
+    let temp = Math.round(response.main.temp - 273.15);
+    let date = (new Date).toString().slice(0,25);
+    let desc = response.weather[0].description.toString()[0].toUpperCase() + response.weather[0].description.slice(1);
+    let humidity = response.main.humidity;
+    let pressure = response.main.pressure;
+    let windSpeed = response.wind.deg;
+    let clouds = response.clouds.all;
+    let icon = parseImg(response.weather[0].icon);
 
 
-        let weatherTodayPattern =
-            '<header>' +
-                '<div class="today-date">' + date + '</div>' +
-            '</header>' +
-            '<div class="today-weather">' +
-                '<div class="today-main">' +
-                    '<div class="temp-icon-wrap">' +
-                        '<div class="today-temp">' + temp + '°C</div>' +
-                        '<img class="today-icon" src="img/' + icon + '.png" alt="Weather Icon"></img>' +
-                    '</div>' +
-                    '<div class="today-description">' + desc + '</div>' +
+
+    let weatherTodayPattern =
+        '<header>' +
+            '<div class="today-date">' + date + '</div>' +
+        '</header>' +
+        '<div class="today-weather">' +
+            '<div class="today-main">' +
+                '<div class="temp-icon-wrap">' +
+                    '<div class="today-temp">' + temp + '°C</div>' +
+                    '<img class="today-icon" src="img/' + icon + '.png" alt="Weather Icon"></img>' +
                 '</div>' +
-                '<aside>' +
-                    '<ul>' +
-                        '<li class="humidity">' + humidity + '%</li>' +
-                        '<li class="pressure">' + pressure + ' hPa</li>' +
-                        '<li class="wind">' + windSpeed + ' m/s</li>' +
-                        '<li class="clouds">' + clouds + '%</li>' +
-                    '</ul>' +
-                '</aside>' +
-            '</div>';
+                '<div class="today-description">' + desc + '</div>' +
+            '</div>' +
+            '<aside>' +
+                '<ul>' +
+                    '<li class="humidity">' + humidity + '%</li>' +
+                    '<li class="pressure">' + pressure + ' hPa</li>' +
+                    '<li class="wind">' + windSpeed + ' m/s</li>' +
+                    '<li class="clouds">' + clouds + '%</li>' +
+                '</ul>' +
+            '</aside>' +
+        '</div>';
 
-        today.innerHTML = weatherTodayPattern;
-    }
+    today.innerHTML = weatherTodayPattern;
+}
 
-    function parseImg(iconCode) {
-        if (iconCode == '03d' || iconCode == '03n' || iconCode == '04n' || iconCode == '04d') {
-            return '03d';
-        } else if (iconCode == '09d' || iconCode == '09n') {
-            return '09d';
-        } else if (iconCode == '10d' || iconCode == '10n') {
-            return '10d';
-        } else if (iconCode == '11d' || iconCode == '11n') {
-            return '11d';
-        } else if (iconCode == '13d' || iconCode == '13n') {
-            return '13d';
-        } else if (iconCode == '50d' || iconCode == '50n') {
-            return '50d';
-        } else {
-            return iconCode;
-        }
-    }
+// function createSelectedByDayPattern() {
 
-    // function createSelectedByDayPattern() {
+// }
 
-    // }
+// function createSelectedByTimePattern() {
+    
+// }
 
-    // function createSelectedByTimePattern() {
-        
-    // }
+// function changeSelectedWeather() {
 
-    // function changeSelectedWeather() {
+// }
 
-    // }
+// <header>
+//     <ul class="timeline">
+//         <li class="time-1"></li>
+//         <li class="time-2"></li>
+//         <li class="time-3"></li>
+//         <li class="time-4"></li>
+//         <li class="time-5"></li>
+//         <li class="time-6"></li>
+//         <li class="time-7"></li>
+//         <li class="time-8"></li>
+//     </ul>
+// </header>
+// <div class="main-weather" data-time='9.00'>
+//     <div class="main-temp"></div>
+//     <div class="main-icon"></div>
+//     <br>
+//     <div class="main-description"></div>
+// </div>
+// <aside>
+//     <ul>
+//         <li class="humidity"></li>
+//         <li class="pressure"></li>
+//         <li class="wind"></li>
+//         <li class="clouds"></li>                        
+//     </ul>
+// </aside>
 
-    // <header>
-    //     <ul class="timeline">
-    //         <li class="time-1"></li>
-    //         <li class="time-2"></li>
-    //         <li class="time-3"></li>
-    //         <li class="time-4"></li>
-    //         <li class="time-5"></li>
-    //         <li class="time-6"></li>
-    //         <li class="time-7"></li>
-    //         <li class="time-8"></li>
-    //     </ul>
-    // </header>
-    // <div class="main-weather" data-time='9.00'>
-    //     <div class="main-temp"></div>
-    //     <div class="main-icon"></div>
-    //     <br>
-    //     <div class="main-description"></div>
-    // </div>
-    // <aside>
-    //     <ul>
-    //         <li class="humidity"></li>
-    //         <li class="pressure"></li>
-    //         <li class="wind"></li>
-    //         <li class="clouds"></li>                        
-    //     </ul>
-    // </aside>
+// function changeWeek (response) {
+//     let fullWeekPattern = '';
 
-    // function changeWeek (response) {
-    //     let fullWeekPattern = '';
+//     for (let i = 1; i < 6; i++) {
 
-    //     for (let i = 1; i < 6; i++) {
+//     }
+// }
 
-    //     }
-    // }
+// function createDayPattern() {
+//     let dayPattern =
+//         '<div class="day-1">' +
+//             '<div class="day-and-data">' +
+//                 '<div class="day-name"></div>' +
+//                 '<div class="data"></div>' +
+//             '</div>' +
+//             '<div class="weather-little">' +
+//                 '<div class="week-temp"></div>' +
+//                 '<div class="week-icon"></div>' +
+//             '</div>' +
+//         '</div>';
 
-    // function createDayPattern() {
-    //     let dayPattern =
-    //         '<div class="day-1">' +
-    //             '<div class="day-and-data">' +
-    //                 '<div class="day-name"></div>' +
-    //                 '<div class="data"></div>' +
-    //             '</div>' +
-    //             '<div class="weather-little">' +
-    //                 '<div class="week-temp"></div>' +
-    //                 '<div class="week-icon"></div>' +
-    //             '</div>' +
-    //         '</div>';
+//     return dayPattern;
+// }
 
-    //     return dayPattern;
-    // }
-
-    return {
-        changeCity: changeCity,
-        changeWeatherToday: changeWeatherToday
+function parseImg(iconCode) {
+    if (iconCode == '03d' || iconCode == '03n' || iconCode == '04n' || iconCode == '04d') {
+        return '03d';
+    } else if (iconCode == '09d' || iconCode == '09n') {
+        return '09d';
+    } else if (iconCode == '10d' || iconCode == '10n') {
+        return '10d';
+    } else if (iconCode == '11d' || iconCode == '11n') {
+        return '11d';
+    } else if (iconCode == '13d' || iconCode == '13n') {
+        return '13d';
+    } else if (iconCode == '50d' || iconCode == '50n') {
+        return '50d';
+    } else {
+        return iconCode;
     }
 }
